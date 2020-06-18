c45582f9595687f41b99b80bfc4e0df0dde7c138
Maxime Robion <max.robion@me.com>
2020-06-18 19:32:49 +0200

    making our own format_categories helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+
+  def format_categories(categories)
+    #get all the categories
+    #turn each one into a link
+    links = categories.map do |cat|
+      link_to cat.title, category_path(cat)
+    end
+    #turn it into a sentence, which is html safe 
+    links.to_sentence.html_safe
+  end
+
 end
