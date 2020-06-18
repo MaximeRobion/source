8d01bff2f6cb8059c43e5f1b95b955a4aebee1b3
Maxime Robion <max.robion@me.com>
2020-06-18 19:24:03 +0200

    making categories and items connections in views

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Item do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :description, :image
+  permit_params :title, :description, :image, category_ids: []
   #
   # or
   #
@@ -14,5 +14,17 @@ ActiveAdmin.register Item do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :image
+      f.input :categories, as: :check_boxes, collection: Category.order("title asc")
+    end
+
+    f.actions
+  end
 
 end
